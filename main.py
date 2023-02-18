6987bd39eda986fc743115c94b1c218ca64b4f1a
qpxdesign <qroshan5@gmail.com>
2023-02-17 21:39:33 -0500

    switched from using scraping to api

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
-from requests_html import HTMLSession
-import re
+import requests
 import os
+import json
 
-session = HTMLSession()
 
-r = session.get('https://github.com/QpxDesign/csclubwesbite')
-aElements = r.html.find("a")
 def getLatestCommitID():
-    for a in aElements:
-        if len(re.findall("/commit/",a.attrs['href'])) != 0:
-            if len(a.text) == 7:
-                return a.text 
-    return ""
-  
+    url = "https://api.github.com/repos/qpxdesign/csclubwesbite/commits"
+    response = requests.get(url)
+    response = response.json()
+    return response[0]["sha"]
+
+
 def getCurrentlyUpCommitID():
     f = open("current_commit_id.txt", "r")
 
+
 def writeCommitID():
-    f  = open("current_commit_id.txt", "w")
+    f = open("current_commit_id.txt", "w")
     f.write(getLatestCommitID())
+
 
 def run():
     os.system("echo standing by")
     if (getLatestCommitID() != getCurrentlyUpCommitID()):
-        writeCommitID();
-        os.system("bash ~/update-portfolio-repos/csclub.sh")
+        writeCommitID()
+        # os.system("bash ~/update-portfolio-repos/csclub.sh")
+
 
 run()
